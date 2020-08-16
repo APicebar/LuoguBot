c832427fe1673a8ab8273ab7a4026ef98ea8bb3b
APicesucker <912370623@qq.com>
2020-08-16 20:26:27 +0800

    修东西

--- etc/plugins/setu.py
+++ etc/plugins/setu.py
@@ -9,6 +9,7 @@ cd = {}
 
 @nonebot.on_command('setu',only_to_me=False)
 async def setu(session: nonebot.CommandSession):
+    cd.get(session.event.user_id, datetime(1970, 1, 1, 0, 0, 0))
     t = datetime.now() - cd[session.event.user_id]
     if t.seconds() < 240:
         await session.send("乖, 不能太贪心哦←_←\n剩余%d秒" % t.seconds())
