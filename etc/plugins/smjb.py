8b7c246b0ab40b315e2aefb2be2434a8b2a55df2
APicesucker <912370623@qq.com>
2020-08-29 13:32:55 +0800

    sb

--- etc/plugins/smjb.py
+++ etc/plugins/smjb.py
@@ -11,9 +11,14 @@ async def fuck(session: nonebot.CommandSession):
     await session.send("去他妈的")
     await bot.set_group_ban(group_id=session.event.group_id, user_id=session.event.user_id, duration=10)
 
+@nonebot.on_command('sb',only_to_me=False)
+async def sb(session: nonebot.CommandSession):
+    await session.send('[CQ:record,file=sb.amr]')
+
 @nonebot.on_command('smjb', only_to_me=False)
 async def help(session: nonebot.CommandSession):
     await session.send('''LuoguBot --- ???
 
 !wuwu --- 呜呜
-!fuck --- 表达愤怒''')
+!fuck --- 表达愤怒
+!sb --- ?''')
