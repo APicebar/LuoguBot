3514748da5d08a1089ff31455b5f2342180ca393
APicesucker <912370623@qq.com>
2020-09-11 23:28:56 +0800

    删了个图源

--- etc/plugins/setu.py
+++ etc/plugins/setu.py
@@ -4,8 +4,7 @@ from urllib import request, error
 import json
 from datetime import timedelta, datetime
 
-Url = ['http://www.dmoe.cc/random.php?return=json',
-    'https://img.paulzzh.tech/touhou/random?proxy=1&size=all&site=all']
+Url = ['http://www.dmoe.cc/random.php?return=json']
 cd = {}
 
 @nonebot.on_command('setu',only_to_me=False)
@@ -15,14 +14,9 @@ async def setu(session: nonebot.CommandSession):
     if t.total_seconds() < 240:
         await session.send("乖, 不能太贪心哦←_←\n剩余%d秒" % (240 - int(t.total_seconds())))
         return
-    site = random.randint(0,1)
-    if site == 0:
-        data = json.loads(request.urlopen(url=Url[site]).read())
-        await session.send("[CQ:image,file=%s]" % data['imgurl'])
-        cd[session.event.user_id] = datetime.now()
-    else:
-        await session.send("[CQ:image,file=%s]" % Url[site])
-        cd[session.event.user_id] = datetime.now()
+    data = json.loads(request.urlopen(url=Url[site]).read())
+    await session.send("[CQ:image,file=%s]" % data['imgurl'])
+    cd[session.event.user_id] = datetime.now()
 
 # @setu.args_parser
 async def _(session: nonebot.CommandSession):
