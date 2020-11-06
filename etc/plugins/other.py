cf1ea3e4aa0b3ecf3b46dfe999c0254fcd3e3a17
APicesucker <912370623@qq.com>
2020-11-07 01:51:43 +0800

    添加broadcast (SU Only)

--- etc/plugins/other.py
+++ etc/plugins/other.py
@@ -1,5 +1,17 @@
 import nonebot
+import aiocqhttp.exceptions as CQHttpError
 
-@nonebot.on_command('feed',only_to_me=False)
+bot = nonebot.get_bot()
+
+@nonebot.on_command('feed', only_to_me=False)
 async def feed(session: nonebot.CommandSession):
     await session.send("[CQ:image,file=feed.jpg]")
+
+@nonebot.on_command('broadcast', only_to_me=False, permission=nonebot.permission.SUPERUSER)
+async def broadcast(session: nonebot.CommandSession):
+    try:
+        g_list = await bot.get_group_list()
+        for i in g_list:
+            await bot.send_group_msg(group_id=i, message=session.current_arg_text)
+    except CQHttpError.ActionFailed:
+        await session.send("失败: 账号可能被风控")
